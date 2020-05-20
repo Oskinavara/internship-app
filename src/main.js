756c1d8c1b9e526484e098aa876f4d7a89b28434
Oskar <oskar@newfantastic.com>
2020-05-21 01:34:25 +0200

    Add some packages to the project

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from "vue";
+import './plugins/axios'
 import App from "./App.vue";
+import store from './store'
+import router from './router'
 
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
+  router,
+
   render: function(h) {
     return h(App);
   }
